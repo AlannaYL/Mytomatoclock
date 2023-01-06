249c0226fc58c89e682bc48541b731253af73903
Alanna <a04810254@gmail.com>
2023-01-06 14:44:46 +0800

    換通知圖片

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -61,7 +61,7 @@ const finishTimer = () => {
     // eslint-disable-next-line
     const notification = new Notification('事項完成', {
       body: currentText.value,
-      icon: 'https://github.com/wdaweb.png'
+      icon: 'https://github.com/AlannaYL.png'
     })
   }
   finish()
